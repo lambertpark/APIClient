627093b3e299995d47a0b79e7335ab7a747cbe89
lambertpark <LambertPark@gmail.com>
2022-03-18 23:15:24 +0900

    Add Get Package

--- APIClient/ViewController.swift
+++ APIClient/ViewController.swift
@@ -21,14 +21,12 @@ class ViewController: UIViewController {
     }
 
     func createUser() async {
-        let client = APIClient(host: "pokeapi.co")
-//        {
-//            $0.sessionConfiguration.httpAdditionalHeaders = ["Content-Type": "application/x-www-form-urlencoded"]
-//            //$0.sessionDelegate = PulseCore.URLSessionProxyDelegate()
-//
-//            // If you also have a session delegate, add it to the delegate chain
-//            //$0.sessionDelegate = PulseCore.URLSessionProxyDelegate(delegate: yourDelegate)
-//        }
+        let client = APIClient(host: "pokeapi.co") {
+            $0.sessionDelegate = PulseCore.URLSessionProxyDelegate()
+
+            // If you also have a session delegate, add it to the delegate chain
+            //$0.sessionDelegate = PulseCore.URLSessionProxyDelegate(delegate: yourDelegate)
+        }
 
         do {
             let pokemons: Pokemons = try await client.send(.get("/api/v2/pokemon")).value
